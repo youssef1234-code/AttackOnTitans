047bf280a3095bfc44fae0592b99564cf31b7927
Omar Gamaleldin <Omar-Gamaleldin@users.noreply.github.com>
2024-02-25 00:26:44 +0200

    Update Titan.java

--- src/game/engine/titans/Titan.java
+++ src/game/engine/titans/Titan.java
@@ -1,6 +1,8 @@
 package game.engine.titans;
 
-abstract public class Titan {
+
+abstract public class Titan implements Comparable{
+	
 	private int baseHealth;
 	private int currentHealth;
 	private int baseDamage;
@@ -10,8 +12,23 @@ abstract public class Titan {
 	private int resourceValue;
 	private int dangerLevel;
 	
-	public	Titan (int baseHealth, int baseDamage, int heightINMeters, int distanceFromBase, int speed, int resourcesValue, int dangerLevel) {
+	public Titan(int baseHealth, int currentHealth, int baseDamage, int heightInMeters, int distanceFrombase, int speed,
+			int resourceValue, int dangerLevel) {
+		
 		this.baseHealth = baseHealth;
+		this.currentHealth = currentHealth;
+		this.baseDamage = baseDamage;
+		this.heightInMeters = heightInMeters;
+		this.distanceFrombase = distanceFrombase;
+		this.speed = speed;
+		this.resourceValue = resourceValue;
+		this.dangerLevel = dangerLevel;
 	}
+	
+	public int compareTo(Titan o) {
+		return this.distanceFrombase - o.distanceFrombase;
+	}
+	
+	
 
 }
